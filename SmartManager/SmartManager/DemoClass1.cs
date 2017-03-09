f3a9327232fada76d5a50bf2cc4569709f8c97b1
avirajlole <avirajlole@gmail.com>
2017-03-09 11:32:53 +0530

    Demo

---
+++ SmartManager/SmartManager/DemoClass1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartManager
+{
+    class DemoClass1
+    {
+
+        // Demo class
+    }
+}
